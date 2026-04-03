3c442cd57cba65eb4eb736502309b9cd2dfeb5c7
mrChickenHead123 <226981646+mrChickenHead123@users.noreply.github.com>
2026-04-03 08:43:00 -0400

    corrected errors

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -4,19 +4,17 @@ from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submi
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
-class QuestionInline(admin.StackedInline)
+class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
 
-class ChoiceInline(admin.StackedInline)
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 2
-
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
-
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -24,7 +22,6 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
-
 
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
