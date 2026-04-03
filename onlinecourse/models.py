912158e19bc776688f8ae9925e9c2e8eb2426e1a
mrChickenHead123 <226981646+mrChickenHead123@users.noreply.github.com>
2026-04-03 07:38:44 -0400

    task 1 up to submission model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -68,6 +68,30 @@ class Course(models.Model):
                "Description: " + self.description
 
 
+# Question model
+class Question(model.Model):
+    content = models.CharField(max_length=200)
+    grade = models.IntegerField(default = 50)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+	# method to calculate if the learner gets the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+    
+    def __str__(self):
+        return "Question: " + self.content
+
+# Choice model
+Class Choice(model.Model):
+    question = models.ForeignKey(ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    is_correct  = models.BooleanField(default=False)
+
 # Lesson model
 class Lesson(models.Model):
     title = models.CharField(max_length=200, default="title")
@@ -98,6 +122,7 @@ class Enrollment(models.Model):
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+# Submission model
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
